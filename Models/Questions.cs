0cc379809f0e966a9b27925ed3a0513d2d51e786
Bimaya Jayamanne <bimayajayamanne@gmail.com>
2024-07-02 22:32:23 +1200

    Add project files.

---
+++ Models/Questions.cs
@@ -0,0 +1,14 @@
+﻿namespace BigBangTheoryTrivia.Models
+{
+    public class Questions
+    {
+        public int Id { get; set; }
+        public string TriviaQuestion { get; set; }
+        public string TriviaAnswer { get; set; }
+
+        public Questions()
+        {
+            
+        }
+    }
+}
